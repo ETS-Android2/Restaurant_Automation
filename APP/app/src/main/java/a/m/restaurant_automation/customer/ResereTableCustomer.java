d852d96817de002d55dc4bc67e71f965fc47be21
Amardeep Grewal <annukaur25101992@gmail.com>
2020-06-01 18:39:36 -0400

    ReserveTableUi

---
+++ APP/app/src/main/java/a/m/restaurant_automation/customer/ResereTableCustomer.java
@@ -0,0 +1,18 @@
+package a.m.restaurant_automation.customer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import a.m.restaurant_automation.R;
+
+public class ResereTableCustomer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_resere_table_customer );
+
+
+    }
+}
