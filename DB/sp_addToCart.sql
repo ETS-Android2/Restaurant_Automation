94441ddaf1cdfb0035058a864468c851189f2c4a
Avtar Singh <32891198+avtargill@users.noreply.github.com>
2020-06-15 16:00:52 -0400

    Stored Procedure Updated to add Cart Items and Order Cart Items

--- DB/sp_addToCart.sql
+++ DB/sp_addToCart.sql
@@ -1,14 +1,14 @@
 USE [restomation]
 GO
-/****** Object:  StoredProcedure [dbo].[addToCart]    Script Date: 2020-05-29 10:14:13 PM ******/
+/****** Object:  StoredProcedure [dbo].[addToCart]    Script Date: 15-06-2020 03:57:40 PM ******/
 SET ANSI_NULLS ON
 GO
 SET QUOTED_IDENTIFIER ON
 GO
 
 
-
-ALTER procedure [dbo].[addToCart]
+--exec addToCart 23,23,5
+CREATE procedure [dbo].[addToCart]
 (
 	@itemId int,
 	@addedBy int,
@@ -19,33 +19,65 @@ BEGIN
 	SET NOCOUNT ON;
 	BEGIN
 		DECLARE @_cartId int = null
+		DECLARE @_avaQTY int = null
+		DECLARE @_itemName varchar(30) = null
+		DECLARE @_itemId int = @itemId
+		DECLARE @_qty int = @qty
+		DECLARE @_updatedQTY int= null;
 	END
 
 	BEGIN
-		Select @_cartId = Cart.cartId from Cart where Cart.menuItemId = @itemId and Cart.isOrdered = 'false' and Cart.addedBy = @addedBy;
+		Select @_cartId = Cart.cartId, @_avaQTY=availableQty, @_itemName=MenuItems.itemName, @_itemId = MenuItems.itemId from Cart JOIN MenuItems on MenuItems.itemId=Cart.menuItemId where Cart.menuItemId = @itemId and Cart.isOrdered = 'false' and Cart.addedBy = @addedBy;
 		if(@_cartId is null)
 		begin
-			Insert into Cart(menuItemId, addedBy, quantity, isOrdered) values (@itemId, @addedBy, @qty, 'false');
-			if(@@ROWCOUNT > 0)
-			begin
-				Select '1' as 'StatusCode', 'Added to cart successfully' as 'StatusMessage';
-			end
+			Select @_avaQTY = MenuItems.availableQty from MenuItems where itemId = @_itemId;
+				if (@_avaQTY >= @_qty)
+				begin
+					Insert into Cart(menuItemId, addedBy, quantity, isOrdered) values (@itemId, @addedBy, @qty, 'false');
+				
+					Select '1' as 'StatusCode', 'Added to cart successfully' as 'StatusMessage';
+				end
+			    else if (@_avaQTY < @qty and @_avaQTY > 0)
+				begin
+					select '404' as 'StatusCode', CONCAT('Sorry, You cannot order more than ',@_avaQTY,' ',@_itemName) as 'StatusMessage';
+				end
+			else if(@_avaQTY = 0)
+				begin
+					select '404' as 'StatusCode',CONCAT('Sorry we are out of ',@_itemName);
+				end
 			else
-			begin
-				Select '0' as 'StatusCode', 'Failed to add to Cart' as 'StatusMessage';
-			end
+				begin
+					Select '01' as 'StatusCode', CONCat('Failed to add to Cart', @_avaQTY,' ',  @_itemId) as 'StatusMessage';
+				end
 		end
 		else
 		begin
-			Update Cart set quantity = (quantity + @qty), addedBy = @addedBy where cartId = @_cartId;
-			if(@@ROWCOUNT > 0)
-			begin
-				Select '1' as 'StatusCode', 'Added to cart successfully' as 'StatusMessage';
-			end
+			Select @_avaQTY = MenuItems.availableQty from MenuItems where itemId = @_itemId;
+			Select @_updatedQTY = (quantity + @qty) from Cart where cartId = @_cartId;
+			if (@_avaQTY >= @_updatedQTY)
+				begin
+					Update Cart set quantity = (quantity + @qty), addedBy = @addedBy where cartId = @_cartId;
+					if(@@ROWCOUNT > 0)
+					begin
+						Select '1' as 'StatusCode', 'Added to cart successfully' as 'StatusMessage';
+					end
+					else
+					begin
+						Select '0' as 'StatusCode', 'Failed to add to Cart' as 'StatusMessage';
+					end
+				end
+			    else if (@_avaQTY < @_updatedQTY and @_avaQTY > 0)
+				begin
+					select '404' as 'StatusCode', CONCAT('Sorry, You cannot order more than ',@_avaQTY,' ',@_itemName) as 'StatusMessage';
+				end
+			else if(@_avaQTY = 0)
+				begin
+					select '404' as 'StatusCode',CONCAT('Sorry we are out of ',@_itemName);
+				end
 			else
-			begin
-				Select '0' as 'StatusCode', 'Failed to add to Cart' as 'StatusMessage';
-			end
+				begin
+					Select '0' as 'StatusCode', 'Failed to add to Cart' as 'StatusMessage';
+				end
 		end
 	END
 END
